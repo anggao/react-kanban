9e32b464252670be2bcaa89fa3729002d54ca89d
Ang Gao <ang.gao87@gmail.com>
2016-02-26 02:20:11 +0000

    add React Babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        }
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            2,
-            2
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
+  'env': {
+    'browser': true,
+    'node': true,
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': 'plugin:react/recommended',
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
     }
+  },
+  'plugins': ['react'],
+  'rules': {
+    'indent': [2, 2],
+    'linebreak-style': [2, 'unix'],
+    'quotes': [2, 'single'],
+    'semi': [2, 'always'],
+    'no-unused-vars': [2, {'varsIgnorePattern': 'React'}],
+    'react/display-name': [0]
+  }
 };
